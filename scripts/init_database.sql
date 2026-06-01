b58a40bddd3f1b56f0b1ceed8ddb309063c25883
Kris98code <kristian98fra@gmail.com>
2026-06-01 02:17:16 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,4 +1,20 @@
 -- Drop and recreate the 'Datawharehouse'
+/*
+=================================================================================
+Create database and Schemas
+=================================================================================
+Script Purpose: 
+    This script is bla bla bla 
+
+
+WARNING: 
+  Running this script will drop the entire 'Datawarehouse' database if it exists. 
+  All data in the database will be permanently deleted. Proceed with caution
+  and ensure you have proper backups before running this script.
+
+
+*/ 
+
 
 IF EXISTS (SELECT 1 FROM databases WHERE name = 'DataWarehouse') 
 BEGIN 
